2686f1249e3789e647b98f626b5c61f5e7686a65
ouyangliang <1105056277@qq.com>
2019-11-01 14:31:20 +0800

    jhjhs

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ let users= [
     {id:1,name:'zhufeng1'},
     {id:2,name:'zhufeng2'},
     {id:3,name:'zhufeng3'},
-    {id:4,name:'zhufeng4'}
+    {id:4,name:'zhufeng4'},
+    {id:5,name:'hahhah'}
+
 ]
 let server = http.createServer(function(req,res){
     res.setHeader('Access-Control-Allow-Origin','*')
