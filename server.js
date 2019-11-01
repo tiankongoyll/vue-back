188ea72fcf42d030e0238311a36177bcc95286b8
ouyangliang <1105056277@qq.com>
2019-11-01 11:55:33 +0800

    sd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let http =require('http')
 let users= [
     {id:1,name:'zhufeng1'},
     {id:2,name:'zhufeng2'},
-    {id:3,name:'zhufeng3'},
+    {id:3,name:'zhufengoo3'},
     {id:4,name:'zhufengouylll4'},
 ]
 let server = http.createServer(function(req,res){
